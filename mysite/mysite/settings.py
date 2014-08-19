aa791f1e160d18ec318e3f1b766af0e6a7c63d19
vanhong <vanhongkao@gmail.com>
2014-08-19 15:33:44 +0800

    update all

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,4 +1,7 @@
+import os
 # Django settings for mysite project.
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,6 +111,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, "templates"),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse, Http404
+from django.template.loader import get_template
+from django.shortcuts import render_to_response
+from django.template import Context
 import datetime
 
 def hello(request):
@@ -6,14 +9,13 @@ def hello(request):
 
 def current_datetime(request):
 	now = datetime.datetime.now()
-	html = "<html><body>It is now %s.</body></html>" %now
-	return HttpResponse(html)
+	return render_to_response('current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
 	try:
 		offset = int(offset)
 	except:
-		return HttpResponse("error")
+		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 	html = "<html><body>In %s hours(s), it will be %s.</body></html>" % (offset, dt)
 	return HttpResponse(html)
